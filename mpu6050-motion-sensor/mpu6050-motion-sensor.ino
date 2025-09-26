a2ee83226c3f41d28312f439e35e2180c2b64c67
Murilo Ferreira <murilo.ferreira32@outlook.com>
2025-09-26 01:03:00 -0300

    minor tweaks at consoles and port from serial

--- mpu6050-motion-sensor/mpu6050-motion-sensor.ino
+++ mpu6050-motion-sensor/mpu6050-motion-sensor.ino
@@ -1,15 +1,15 @@
-// Lib to communcate with sensor MPU 
-#include <MPU6050_tockn.h>
 // Lib to use communication e2e
 #include <Wire.h>
+// Lib to communcate with sensor MPU 
+#include <MPU6050_tockn.h>
 
 MPU6050 mpu6050(Wire);
 
 void setup() {
-  //Set Port
-  Serial.begin(11500);
-  //Initialize communication e2e
-  Wire.begin();
+  //Set Port ##115200 is better to ESP8266 debbuger
+  Serial.begin(115200);
+  //Initialize communication I2E
+  Wire.begin(D2,D1); //Ports SDA AND SCL
   //Initialize MPU
   mpu6050.begin();
   //Calibrates the sensor ##Important: The component must be a flat surface to calibrate
@@ -19,11 +19,15 @@ void setup() {
 
 void loop() {
   mpu6050.update();
-  Serial.print("Angle X: ");
+
+  Serial.print("1 - Angle X: ");
   Serial.print(mpu6050.getAngleX());
-  Serial.print("Angle: Y: ");
+  Serial.print("    2 - Angle: Y: ");
   Serial.print(mpu6050.getAngleY());
-  Serial.print("Angle Z: ");
+  Serial.print("    3 - Angle Z: ");
   Serial.print(mpu6050.getAngleZ());
-  Serial.print("--------Done--------");
+
+  Serial.println();//Break line
+
+  delay(500);
 }
